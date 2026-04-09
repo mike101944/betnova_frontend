3fbc0b4e14f5959e84739bc925f0c33f4dd0f99b
ambokileB <wholemike2703email.com>
2026-04-09 11:37:08 +0300

    feat:changes

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 // http://localhost:5000/api
 // Create axios instance
 const api = axios.create({
-  baseURL: 'http://5.189.136.33/api/api',
+  baseURL: 'https://betnover.com/api/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
