b35d4fed8cb0455726502e34416362d0db1bca6f
ambokileB <wholemike2703email.com>
2026-05-07 11:15:14 +0300

    feat:changes made

--- src/services/api.js
+++ src/services/api.js
@@ -35,7 +35,7 @@ api.interceptors.response.use(
     if (error.response?.status === 401) {
       localStorage.removeItem('access_token')
       localStorage.removeItem('refresh_token')
-      window.location.href = '/login'
+      window.location.href = '/'
     }
     return Promise.reject(error)
   }
